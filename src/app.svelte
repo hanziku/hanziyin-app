f49e0160a17b4174bf78750b1cac9fb1813fcd14
yapcheahshen@gmail.com <yapcheahshen@gmail.com>
2021-08-25 08:47:05 +0630

    testing pwa

--- src/app.svelte
+++ src/app.svelte
@@ -4,12 +4,19 @@ import StackView from "./stackview.svelte"
 import Notification from "./notification.svelte"
 import QuickInput from "./quickinput.svelte"
 import { addHZ } from "./store";
+import {getPWADisplayMode,registerServiceWorker} from 'kaigua'
+
+registerServiceWorker();
+
 const setTofind=()=>{
 	addHZ('漢','tofind',-1);
 }
+
 </script>
 <div class="main">
-	<span class="title" on:click={setTofind}>字引</span><QuickInput/><Notification/>
+	<span class="title" on:click={setTofind}>汉字引</span>
+	<QuickInput/><Notification/>
+
 	<HanziyinPicker/>
 	<StackView/>
 
